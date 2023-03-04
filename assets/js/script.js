4bb8ab781f9b0a5e4d33fba7d203dd2672faa8b7
addas3 <addas3code@gmail.com>
2023-03-04 14:44:06 +0000

    Add the division section

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,6 +50,8 @@ function runGame(gameType) {
     } else if (gameType === "subtract") {
         displaySubtractQuestion (num1, num2); 
 
+    } else if (gameType === "division") {
+		displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unkown game type ${gameType}`);
         throw `Unkown game type: ${gameType}. Aborting!`;
@@ -96,6 +98,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     }  else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator ==="/") {
+		return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`
@@ -144,5 +148,13 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+	document.getElementById("operand1").textContent = (operand1 * operand2)
+	document.getElementById("operand2").textContent =  operand2;
+	document.getElementById("operator").textContent = "/";
 
 }
